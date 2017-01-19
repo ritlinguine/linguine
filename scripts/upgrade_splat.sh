022d6a80f1a66254b1bc7576055f250b2c164548
meyersbs <meyers.bs@gmail.com>
2017-01-19 12:07:50 -0500

    Updated upgrade_splat to resolve dependencies

--- scripts/upgrade_splat.sh
+++ scripts/upgrade_splat.sh
@@ -1,1 +1,9 @@
+# SPLAT
 sudo /home/linguini/.pyenv/versions/3.4.3/bin/pip3.4 install --upgrade SPLAT-library
+
+# Resolve Dependencies
+sudo /home/linguini/.pyenv/versions/3.4.3/bin/pip3.4 install --upgrade nltk
+sudo /home/linguini/.pyenv/versions/3.4.3/bin/pip3.4 install --upgrade psutil
+sudo /home/linguini/.pyenv/versions/3.4.3/bin/pip3.4 install --upgrade matplotlib
+
+
