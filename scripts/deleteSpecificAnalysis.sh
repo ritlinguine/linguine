228cd5ee6b609df98062821fcfd9a92b5296f232
meyersbs <meyers.bs@gmail.com>
2017-05-10 15:13:29 -0400

    Tested and Working.

--- scripts/deleteSpecificAnalysis.sh
+++ scripts/deleteSpecificAnalysis.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-USER = ${1}
-NAME = ${2}
+USER=${1}
+NAME=${2}
 
-echo -e "use linguine-development\ndb.analyses.remove({\"user_id\":db.users.findOne({\"dce\":\"\$USER\"})._id,\"analysis_name\":\"\$NAME\"})"
+echo -e "use linguine-development\ndb.analyses.remove({\"user_id\":db.users.findOne({\"dce\":\"$USER\"})._id,\"analysis_name\":\"$NAME\"})" | mongo
