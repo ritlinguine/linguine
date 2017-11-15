21ce844bac65fa8516730aff3de21179761033a6
meyersbs <meyers.bs@gmail.com>
2017-11-14 22:44:32 -0500

    Fixed typo.

--- scripts/deleteFailedAnalyses.sh
+++ scripts/deleteFailedAnalyses.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo -e "use linguine-development\ndb.analyses.remove({\"result\":\"\"})"
-echo -e "use linguine-production\ndb.analyses.remove({\"result\":\"\"})"
+echo -e "use linguine-development\ndb.analyses.remove({\"result\":\"\"});" | mongo
+echo -e "use linguine-production\ndb.analyses.remove({\"result\":\"\"});" | mongo
