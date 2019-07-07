2cc88a013f418af10ab803edae3d4fd5deccd9db
aphedges <aphedges1@gmail.com>
2019-07-07 17:59:27 -0400

    Added linguine-corenlp to restart scripts

--- scripts/restart-nightly.sh
+++ scripts/restart-nightly.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+systemctl restart linguine-corenlp
 systemctl restart linguine-node
 systemctl restart linguine-python
 systemctl restart linguine-sphinx
--- scripts/restart-on-high-load.sh
+++ scripts/restart-on-high-load.sh
@@ -4,6 +4,7 @@ CPU_USAGE=$(uptime | cut -d"," -f5 | cut -d":" -f2 | cut -d" " -f2 | sed -e "s/\
 CPU_USAGE_THRESHOLD=200
 
 if [[ 10#${CPU_USAGE} -gt 10#${CPU_USAGE_THRESHOLD} ]]; then
+  systemctl restart linguine-corenlp
   systemctl restart linguine-node
   systemctl restart linguine-python
   systemctl restart linguine-sphinx
